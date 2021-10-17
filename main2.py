20fa17fbb9b2f2f43434a717e93adf7fe8225105
Anna Evstifeikina <an_evstif@mail.ru>
2021-10-17 15:58:42 +0300

    еще чего-то не хватает при вычислениях, большая погрешность

--- main2.py
+++ main2.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
 list_in=[]
 s=''
-f = open('data2.csv')
-f.readline()
-for str in f:
-    s = str.strip().split(',')
-    print(s)
-    a = list(map(float, s))
-    list_in.append(a)
-f.close()
+f = pd.read_csv('data.csv')
+X = np.array(f.get('km'))
+Y = np.array(f.get('price'))
+
+X = (X  - X.mean())/X.std()
+
 tmp_Q_0 = 0.0
 tmp_Q_1 = 0.0
 learningRate = 0.05
@@ -16,14 +18,14 @@ learningRate = 0.05
 def estimatePrice(mileage, q_0, q_1):
     return (q_0 + (q_1 * mileage))
 
-for i in range(10):
+for i in range(100):
     sum0 = 0.0
     sum1 = 0.0
-    for j in range(len(list_in)):
-        sum0 = sum0 + estimatePrice(list_in[j][0], tmp_Q_0, tmp_Q_1) - list_in[j][1]
-        sum1 = sum1 + (estimatePrice(list_in[j][0], tmp_Q_0, tmp_Q_1) - list_in[j][1]) * list_in[j][0]
-    tmp_Q_0 = learningRate * sum0 / len(list_in)
-    tmp_Q_1 = learningRate * sum1 / len(list_in)
+    for j in range(len(X)):
+        sum0 = sum0 + estimatePrice(X[j], tmp_Q_0, tmp_Q_1) - Y[j]
+        sum1 = sum1 + (estimatePrice(X[j], tmp_Q_0, tmp_Q_1) - Y[j]) * X[j]
+    tmp_Q_0 = learningRate * sum0 / len(X)
+    tmp_Q_1 = learningRate * sum1 / len(X)
     print(f'{tmp_Q_0}, {tmp_Q_1}')  
     
 with open('tmp.csv', 'w') as f_k:
