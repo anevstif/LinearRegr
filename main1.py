009f056d0bfc804dc6dc6e2c5b2b7b7a97878b5f
Anna Evstifeikina <an_evstif@mail.ru>
2021-10-18 17:30:46 +0300

    + обработка ввода

--- main1.py
+++ main1.py
@@ -1,16 +1,29 @@
 def estimatePrice(mileage, q_0, q_1):
     return (q_0 + (q_1 * mileage))
 
-list_in=[]
 s=''
-f = open('tmp.csv')
-for str in f:
-    s = str.strip().split(',')
-    print(s)
-    a = list(map(float, s))
+with open('tmp.csv') as f :
+    try :
+        for str in f:
+            s = str.strip().split(',')
+            a = list(map(float, s))
+        if len(a) != 2 : OK = 1
+    except :
+        exit('File with theta is wrong')
 f.close()
-print('Введите пробег в милях')
-str = input() 
-mileage = int(str.strip('-'))
-print('Прогнозируемая стоимость')
+
+
+while True :
+    print('Введите пробег в милях (целое число)')
+    str = input() 
+    try : 
+        mileage = int(str.strip())
+        if mileage > 0 :
+            break
+        else :
+            print('Введите положительное число')
+    except :
+            print('Введие положительное число')
+
+print('Прогнозируемая стоимость автомобиля:')
 print(f'{estimatePrice(mileage, a[0], a[1])}')
--- main2.py
+++ main2.py
@@ -9,12 +9,12 @@ f = pd.read_csv('data.csv')
 X = np.array(f.get('km'))
 Y = np.array(f.get('price'))
 
-X = (X  - X.mean()) / X.std()
+Xnorm = (X  - X.mean()) / X.std()
 
 tmp_Q_0 = 0.0
 tmp_Q_1 = 0.0
 learningRate = 0.05
-iter = 100
+iter = 1000
 
 def estimatePrice(mileage, q_0, q_1):
     return (q_0 + (q_1 * mileage))
@@ -23,17 +23,18 @@ for i in range(iter):
     sum0 = 0.0
     sum1 = 0.0
     for j in range(len(X)):
-        sum0 = sum0 + estimatePrice(X[j], tmp_Q_0, tmp_Q_1) - Y[j]
-        sum1 = sum1 + (estimatePrice(X[j], tmp_Q_0, tmp_Q_1) - Y[j]) * X[j]
-    tmp_Q_0 = learningRate * sum0 / len(X)
-    tmp_Q_1 = learningRate * sum1 / len(X)
+        sum0 = sum0 + estimatePrice(Xnorm[j], tmp_Q_0, tmp_Q_1) - Y[j]
+        sum1 = sum1 + (estimatePrice(Xnorm[j], tmp_Q_0, tmp_Q_1) - Y[j]) * Xnorm[j]
+    tmp_Q_0 = tmp_Q_0 - learningRate * sum0 / len(X)
+    tmp_Q_1 = tmp_Q_1 - learningRate * sum1 / len(X)
     print(f'{tmp_Q_0}, {tmp_Q_1}')  
     
 with open('tmp.csv', 'w') as f_k:
     f_k.write(f'{tmp_Q_0},{tmp_Q_1}')
 
 plt.plot(X, Y, 'b.')
-plt.plot(X, tmp_Q_0 + X * tmp_Q_1)
+plt.plot(X, tmp_Q_0 + Xnorm  * tmp_Q_1)
 plt.xlabel('km')
 plt.ylabel('price')
+plt.title('Regr')
 plt.show()
